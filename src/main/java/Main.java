0bcc5d8b27037f2387375ac315119f9c965a2e26
kwon15477 <kbj7072@naver.com>
2026-01-29 14:30:42 +0900

    1 단계

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==명언 앱==");
+        System.out.print("명령) ");
+        String cmd = sc.nextLine();
+
+
+    }
+}
